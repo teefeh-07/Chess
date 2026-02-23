c060b772c3e149a065616e143852ce8cb3b1d4aa
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-23 17:03:26 +0100

    feat(wallet): implement wallet connect initialization

--- src/lib/wallet/wallet-connect.ts
+++ src/lib/wallet/wallet-connect.ts
@@ -1,4 +1,12 @@
 import { SignClient } from "@walletconnect/sign-client";
 export const initWalletConnect = async () => {
-  console.log("init");
+  return await SignClient.init({
+    projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID,
+    metadata: {
+      name: "Chess dApp",
+      description: "Web3 Chess game",
+      url: "https://chess-dapp.xyz",
+      icons: ["https://chess-dapp.xyz/icon.png"]
+    }
+  });
 };
