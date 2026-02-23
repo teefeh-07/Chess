618503e089d38698ff9fea8177d27d9f2b9d875d
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-23 18:20:07 +0100

    feat: implement leaderboard container skeleton

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -4,3 +4,7 @@ import { PlayerProfile } from "../services/matchmaking";
 interface LeaderboardProps {
   players: PlayerProfile[];
 }
+
+export const Leaderboard: React.FC<LeaderboardProps> = ({ players }) => {
+  return <div className="leaderboard-container"></div>;
+};
